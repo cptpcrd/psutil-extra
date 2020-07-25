8cb58fbb87a05a0721a5b5623cedc35f36e60a4a
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-07-25 08:02:12 -0400

    _pssolaris: Use *id_t types from _ffi instead of redefining

--- psutil_extra/_pssolaris.py
+++ psutil_extra/_pssolaris.py
@@ -1,4 +1,3 @@
-# pylint: disable=invalid-name
 import contextlib
 import ctypes
 import errno
@@ -8,17 +7,14 @@ import psutil
 
 from . import _ffi
 
-pid_t = ctypes.c_int
-gid_t = ctypes.c_uint32
-
 libc = _ffi.load_libc()
 
-libc.ucred_get.argtypes = (pid_t,)
+libc.ucred_get.argtypes = (_ffi.pid_t,)
 libc.ucred_get.restype = ctypes.c_void_p
 
 libc.ucred_getgroups.argtypes = (
     ctypes.c_void_p,
-    ctypes.POINTER(ctypes.POINTER(gid_t)),  # pytype: disable=invalid-typevar
+    ctypes.POINTER(ctypes.POINTER(_ffi.gid_t)),  # pytype: disable=invalid-typevar
 )
 libc.ucred_getgroups.restype = ctypes.c_int
 
@@ -43,7 +39,7 @@ def proc_getgroups(pid: int) -> List[int]:
         raise psutil.NoSuchProcess(pid)
 
     with _get_ucred(pid) as raw_ucred:
-        groups_ptr = ctypes.POINTER(gid_t)()  # pytype: disable=not-callable
+        groups_ptr = ctypes.POINTER(_ffi.gid_t)()  # pytype: disable=not-callable
         ngroups = libc.ucred_getgroups(raw_ucred, ctypes.pointer(groups_ptr))
         if ngroups < 0:
             raise _ffi.build_oserror(errno.EINVAL)
